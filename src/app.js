e1600ac5d10b892e919f9da904b4872e1f5dcba6
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-17 00:43:21 +0530

    feat: set up express server with basic configuration and dependencies
    - Added "type": "module" to package.json for ES module support
    - Installed new dependencies: cors, dotenv, express, and pino
    - Created Logger.js utility file
    - Implemented basic express server in app.js with a root route

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log("hello node");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+const PORT = process.env.PORT || "8090";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h2>Library management system api</h2>");
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`server is up and running on port ${PORT}`);
+});
